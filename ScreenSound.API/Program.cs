6fd560bb8b1678cb2c1cb22cff8d7c03c3b87955
Bronze <joaobronze@sis-it.com>
2024-01-27 22:22:41 -0300

    ScreenSound

---
+++ ScreenSound.API/Program.cs
@@ -0,0 +1,39 @@
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+    // Configurar a cultura invariante
+    CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+    CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    return dal.Listar();
+
+});
+
+app.Run();
+
---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    public class ScreenSoundContext : DbContext
+    {
+        public DbSet<Artista> Artistas { get; set; }
+        public DbSet<Musica> Musicas { get; set; }
+
+        private string connectionString = "Data Source=localhost;Initial Catalog=ScreenSound;User ID=SA;Password=Bagu@633710;TrustServerCertificate=true;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configurar o contexto do banco de dados
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+        }
+    }
+}
