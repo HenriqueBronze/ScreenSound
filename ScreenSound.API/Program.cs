398affcad9a2643067b69d8b4a7b259404bc1900
Bronze <joaobronze@sis-it.com>
2024-01-28 14:59:06 -0300

    crud de musicas

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System;
@@ -11,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
 
 
 // Add services to the container.
@@ -30,6 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+#region Endpoint Artistas
 app.MapGet("/Artistas", async ([FromServices]DAL<Artista> dal) =>
 {
     return Results.Ok(dal.Listar());
@@ -74,10 +77,11 @@ app.MapDelete("/Artistas/{id}", async ([FromServices] DAL<Artista> dal, int id) 
     {
         return Results.NotFound();
     }
-    
+
     dal.Deletar(artista);
 
     return Results.NoContent();
+
 
 });
 
@@ -95,7 +99,70 @@ app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista art
     dal.Atualizar(artistaAAtualizar);
     return Results.Ok();
 });
+#endregion
+
+#region Endpoint Músicas
+app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
+{
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", async  ([FromServices] DAL<Musica> dal, HttpContext context, string nome) =>
+{
+    try
+    {
+        using (var reader = new StreamReader(context.Request.Body))
+        {
+            var json = await reader.ReadToEndAsync();
+            var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+            if (artista is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync($"Artista {nome} não encontrato");
+            }
+            else
+                await context.Response.WriteAsJsonAsync(artista);
+        }
+    }
+    catch (JsonException ex)
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        await context.Response.WriteAsync($"Erro ao desserializar JSON: {ex.Message}");
+    }
 
 
+});
+
+app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    dal.Adicionar(musica);
+    return Results.Ok();
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
+    var musica = dal.RecuperarPor(a => a.Id == id);
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    dal.Deletar(musica);
+    return Results.NoContent();
+
+});
+
+app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
+    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
+    if (musicaAAtualizar is null)
+    {
+        return Results.NotFound();
+    }
+    musicaAAtualizar.Nome = musica.Nome;
+    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
+
+    dal.Atualizar(musicaAAtualizar);
+    return Results.Ok();
+});
+#endregion
 app.Run();
 
